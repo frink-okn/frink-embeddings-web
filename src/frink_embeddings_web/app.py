a1e577751bafc1efb1fa125f9b2206265d20d8c2
Patrick Golden <ptgolden@email.unc.edu>
2026-01-15 12:41:34 -0500

    Log detected settings on startup

--- src/frink_embeddings_web/app.py
+++ src/frink_embeddings_web/app.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from frink_embeddings_web.context import AppContext, WrappedFlask
 from frink_embeddings_web.settings import load_settings
 
@@ -6,6 +8,9 @@ def create_app() -> WrappedFlask:
     app = WrappedFlask(__name__)
 
     settings = load_settings()
+
+    logger.info("Detected settings: " + str(settings))
+
     ctx = AppContext.from_settings(settings)
     app.ctx = ctx
 
