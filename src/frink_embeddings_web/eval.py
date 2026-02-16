f8b4eba4089df138ac3ebc6c09d76ae2feb223ea
Patrick Golden <ptgolden@email.unc.edu>
2026-02-16 12:09:07 -0500

    Output evaluation queries to a JSON file
    ...reports and visualizations can be shown later...

--- src/frink_embeddings_web/eval.py
+++ src/frink_embeddings_web/eval.py
@@ -2,13 +2,16 @@ import tomllib
 from pathlib import Path
 
 import typer
+from loguru import logger
 from pydantic import BaseModel
-from qdrant_client.models import ScoredPoint
+from qdrant_client.models import CollectionInfo, ScoredPoint
 
 from frink_embeddings_web.context import AppContext
 from frink_embeddings_web.model import Query, TextFeature
 from frink_embeddings_web.query import run_similarity_search
 from frink_embeddings_web.settings import load_settings
+
+app = typer.Typer(pretty_exceptions_enable=False)
 
 
 class EvalConfig(BaseModel):
@@ -40,8 +43,25 @@ def fmt_range(points: list[ScoredPoint]):
     return f"{points[-1].score:.2f} to {points[0].score:.2f}"
 
 
+class QueryResult(BaseModel):
+    target_graph: str
+    query: str
+    knn_results: list[ScoredPoint]
+    ann_ingraph_results: list[ScoredPoint]
+    ann_allgraph_results: list[ScoredPoint]
+
+
+class Evaluation(BaseModel):
+    collection_settings: CollectionInfo
+    hnsw_ef: int
+    queries: list[QueryResult]
+    pass
+
+
+@app.command()
 def run_eval(
     queries_toml: Path,
+    output: Path,
 ):
     settings = load_settings()
     settings.qdrant_timeout = 60
@@ -50,70 +70,84 @@ def run_eval(
 
     limit = 50
 
+    collection_settings = ctx.client.get_collection(
+        ctx.settings.qdrant_collection
+    )
+
+    evaluation = Evaluation(
+        collection_settings=collection_settings,
+        hnsw_ef=settings.qdrant_hnsw_ef,
+        queries=[],
+    )
+
     for graph, terms in config.queries.items():
         for term in terms:
-            print(f"evaluating query '{term}' in graph '{graph}'")
+            logger.info(f"evaluating query '{term}' in graph '{graph}'")
+
             # Exact k-nearest neighbor search
             knn_results = run_similarity_search(
+                ctx,
                 query_obj=Query(
                     include_graphs=[graph],
                     feature=TextFeature(type="text", value=term),
                     limit=limit,
                 ),
-                client=ctx.client,
-                model=ctx.model,
-                collection_name=settings.qdrant_collection,
                 exact=True,
-                hnsw_ef=settings.qdrant_hnsw_ef,
             )
 
             # Approximate nearest neighbor search
             ann_results = run_similarity_search(
+                ctx,
                 query_obj=Query(
                     include_graphs=[graph],
                     feature=TextFeature(type="text", value=term),
                     limit=limit,
                 ),
-                client=ctx.client,
-                model=ctx.model,
-                collection_name=settings.qdrant_collection,
-                exact=False,
-                hnsw_ef=settings.qdrant_hnsw_ef,
             )
 
             # Approximate nearest neighbor search without limiting to graph
             all_results = run_similarity_search(
+                ctx,
                 query_obj=Query(
                     feature=TextFeature(type="text", value=term),
                     limit=limit,
                 ),
-                client=ctx.client,
-                model=ctx.model,
-                collection_name=settings.qdrant_collection,
-                exact=False,
-                hnsw_ef=settings.qdrant_hnsw_ef,
             )
 
-            recall, recall_overlap = recall_at_k(knn_results, ann_results)
-            recall_full, recall_full_verlap = recall_at_k(
-                knn_results, all_results
+            result = QueryResult(
+                target_graph=graph,
+                query=term,
+                knn_results=knn_results,
+                ann_ingraph_results=ann_results,
+                ann_allgraph_results=all_results,
             )
 
-            all_results_min = all_results[-1].score
-            missing_points = [
-                point for point in knn_results
-                if point.score > all_results_min and
-                point.id not in get_ids(all_results)
-            ]
-            print(f"recall@{limit} for in-graph ANN results: {recall}")
-            print(f"recall@{limit} for all-graph ANN results: {recall_full}")
-            print("Cosine similarity ranges:")
-            print(f"  KNN:      {fmt_range(knn_results)}")
-            print(f"  ANN:      {fmt_range(ann_results)}")
-            print(f"  Full ANN: {fmt_range(all_results)}")
-            print(f"missed points in full ANN: {len(missing_points)}")
-            print()
+            evaluation.queries.append(result)
+
+            # recall, recall_overlap = recall_at_k(knn_results, ann_results)
+            # recall_full, recall_full_verlap = recall_at_k(
+            #     knn_results, all_results
+            # )
+
+            # all_results_min = all_results[-1].score
+            # missing_points = [
+            #     point
+            #     for point in knn_results
+            #     if point.score > all_results_min
+            #     and point.id not in get_ids(all_results)
+            # ]
+            # print(f"recall@{limit} for in-graph ANN results: {recall}")
+            # print(f"recall@{limit} for all-graph ANN results: {recall_full}")
+            # print("Cosine similarity ranges:")
+            # print(f"  KNN:      {fmt_range(knn_results)}")
+            # print(f"  ANN:      {fmt_range(ann_results)}")
+            # print(f"  Full ANN: {fmt_range(all_results)}")
+            # print(f"missed points in full ANN: {len(missing_points)}")
+            # print()
+
+    with output.open("w") as fp:
+        fp.write(evaluation.model_dump_json(indent=2))
 
 
 if __name__ == "__main__":
-    typer.run(run_eval)
+    app()
